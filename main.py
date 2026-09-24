4c3a18599feed6c50f6b4dedeec8ad94ba7e4ad5
24lannonj-cmd <24lannonj@cedarsupper.co.uk>
2026-09-24 21:30:18 +0100

    Increase decayRate for smoother speed reduction

--- main.py
+++ main.py
@@ -728,7 +728,7 @@ HTML_CLIENT = """
                         me.vz *= scale;
                     } else {
                         // Smoothly bleed off excess speed down to 50 over ~1.5 seconds when releasing Shift
-                        const decayRate = 0.85;
+                        const decayRate = 0.99;
                         me.vx *= decayRate;
                         me.vy *= decayRate;
                         me.vz *= decayRate;
