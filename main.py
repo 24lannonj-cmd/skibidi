64d1bd536c0a8678500e525125bc5ba5d4c76a8c
24lannonj-cmd <24lannonj@cedarsupper.co.uk>
2026-09-25 10:48:44 +0100

    Update OBJ file URL for ship model

--- main.py
+++ main.py
@@ -110,7 +110,7 @@ HTML_CLIENT = """
         const objLoader = new THREE.OBJLoader();
         
         // Update URL to match your actual file location
-        objLoader.load('/static/ship.obj', function (obj) {
+        objLoader.load('https://raw.githubusercontent.com/24lannonj-cmd/skibidi/main/ship.obj', function (obj) {
             const shipMaterial = new THREE.MeshStandardMaterial({ 
                 color: 0x00aaff, 
                 metalness: 0.8, 
